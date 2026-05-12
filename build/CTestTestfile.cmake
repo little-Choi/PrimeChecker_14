7439da6369d8150cc7b75f791089636840d6000f
little-Choi <dkssudsdkssu@gmail.com>
2026-05-12 15:36:45 +0900

    First Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/DEV/PrimeChecker_14
+# Build directory: C:/DEV/PrimeChecker_14/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(PrimeCheckerTest "C:/DEV/PrimeChecker_14/build/prime_checker_test.exe")
+set_tests_properties(PrimeCheckerTest PROPERTIES  _BACKTRACE_TRIPLES "C:/DEV/PrimeChecker_14/CMakeLists.txt;17;add_test;C:/DEV/PrimeChecker_14/CMakeLists.txt;0;")
